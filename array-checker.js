d80046041c2b3534c53500dd55c7a5beac31b5eb
Hoda <hs@Hodas-MacBook-Air.local>
2023-04-03 15:31:12 +0200

    fixed issues in the last solution

--- array-checker.js
+++ array-checker.js
@@ -1,7 +1,13 @@
 function uniqueCalc(arr1, arr2) {
-  uniqueArr = [...new Set(arr1.filter((item) => arr2.includes(item)))];
+  let unique1 = arr1.filter((o) => arr2.indexOf(o) === -1);
+  let unique2 = arr2.filter((o) => arr1.indexOf(o) === -1);
+  let sum = 0;
+  const unique = unique1.concat(unique2);
 
-  console.log(uniqueArr);
+  for (let i = 0; i < unique.length; i++) {
+    sum += unique[i];
+  }
+  console.log(` the unique numbers are : ${unique}, and their sum is: ${sum}`);
 }
 
 uniqueCalc([3, 1, 7, 9], [2, 4, 1, 9, 3]);
