2a621663002075d59fec28d65219f091170a7b65
Hoda <hs@Hodas-MacBook-Air.local>
2023-04-03 15:03:09 +0200

    fixed the javascript solution

--- array-checker.js
+++ array-checker.js
@@ -1,15 +1,7 @@
 function uniqueCalc(arr1, arr2) {
-  for (var i = 0; i < arr1.length; i++) {
-    for (var j = 0; j < arr2.length; j++) {
-      if (arr1[i] === arr2[j]) {
-        arr1 = arr1.splice(i, 1);
-      }
-    }
-    var newArr = [...arr1, ...arr2];
-    var total = 0;
-    for (var i = 0; i < newArr.length; i++) {
-      total += newArr[i];
-    }
-    return total;
-  }
+  uniqueArr = [...new Set(arr1.filter((item) => arr2.includes(item)))];
+
+  console.log(uniqueArr);
 }
+
+uniqueCalc([3, 1, 7, 9], [2, 4, 1, 9, 3]);
